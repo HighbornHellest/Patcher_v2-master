20007c8b1255fac2a56a878fe2d99d9fe8e0e548
HighbornHellest <svel.public@gmail.com>
2021-01-02 19:24:07 +0100

    settings 0.1
    settings are saving

--- Patcher/Patcher/Settings.xaml.cs
+++ Patcher/Patcher/Settings.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace Patcher
 {
@@ -24,9 +25,43 @@ namespace Patcher
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        //this is the cancel
+        private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             this.NavigationService.Navigate(main_window.launcher);
         }
+
+        //this is the accept
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            String[] res = Resolution.Text.Split('x');
+            ushort freq = UInt16.Parse(Frequency.Text);
+            ushort gamma = UInt16.Parse(Gamma.Text);
+            string windowed="0";
+
+            if (radio_windowed.IsChecked == true)
+            {
+                windowed = "1";
+            }
+            if (radio_full.IsChecked == true)
+            {
+                windowed = "0";
+            }
+
+            String[] config_lines={"WIDTH"+" "+ res[0],"HEIGHT"+" "+res[1], "BPP"+" "+"32",
+                "FREQUENCY"+ " "+ Frequency.Text,
+                "WINDOWED"+" " + windowed };
+
+            File.Delete(@".\metin");
+            string path = @".\metin";
+            File.WriteAllLines(path, config_lines);
+
+
+            
+
+            this.NavigationService.Navigate(main_window.launcher);
+        }
+
+        
     }
 }
